b02be320d81b0fab3e8b01b70445aa8121279e09
benjadahl <benjadahl@gmail.com>
2016-08-17 22:37:11 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,13 @@
+function eventFire(el, etype){
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
+$(document).ready(function(){
+  //alert("yays");
+});
