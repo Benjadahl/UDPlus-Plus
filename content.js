edc169824278ab60b99a96c64f4dd16e7c468a58
benjadahl <benjadahl@gmail.com>
2016-08-20 23:52:08 +0200

    Added the files from the personal development repository, which will now become this branch instead

--- content.js
+++ content.js
@@ -9,5 +9,9 @@ function eventFire(el, etype){
 }
 
 $(document).ready(function(){
-  //alert("yays");
+  console.log("Uddata++ starting");
+  $(".navbar-inner").css("background-color","red");
+  $(".ace-nav>li>a>[class*='icon-']").css("color","red");
+  $(".ace-nav>li.light-blue").css("background","red");
+  $("#navbar>div>div>a>img").attr("src", chrome.extension.getURL("UddataLogo.png"));
 });
