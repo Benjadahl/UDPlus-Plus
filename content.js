e97c3a90f5dec1cd34387302d6c671653e2feb82
benjadahl <benjadahl@gmail.com>
2016-08-21 16:52:51 +0200

    Removed unused code snippet

--- content.js
+++ content.js
@@ -1,13 +1,3 @@
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
-
 $(document).ready(function(){
   console.log("Uddata++ starting");
   $(".navbar-inner").css("background-color","red");
