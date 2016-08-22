e101e80f73cb51a13e58d9d81fef6dfef6198874
jbjjbjjbj <julianteule@gmail.com>
2016-08-22 13:20:49 +0200

    Atom stuff

--- popup/popup.js
+++ popup/popup.js
@@ -4,27 +4,27 @@ var themes = {"dark" : {"foreground": "001100", "text" : "110000", "background":
 document.addEventListener('DOMContentLoaded', function() {
   var themeSelect = document.getElementById('theme');
 	var fontSelect = document.getElementById('font');
-	
+
 	chrome.storage.sync.get('theme', function (obj) {
 		if (!chrome.runtime.error) {
 			themeSelect.value = obj.theme;
 		}
 	});
-	
+
 	chrome.storage.sync.get('font', function (obj) {
 		if (!chrome.runtime.error) {
 			fontSelect.value = obj.font;
 		}
 	});
-	
+
 	themeSelect.addEventListener('change', function() {
 		chrome.storage.sync.set({'theme' : theme.value});
 		chrome.storage.sync.set(themes[theme.value]);
-		
+
 	}, false);
-	
+
 	fontSelect.addEventListener('change', function() {
 		chrome.storage.sync.set({'font' : fontSelect.value});
 	}, false);
-	
+
 }, false);
