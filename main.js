73a6096898a5beffb0fb41776bd3dd0d50c49cca
Rasmus Kjær <rasmase@gmail.com>
2016-08-25 19:35:33 +0200

    Detects the keyword lektie and makes the lesson transparent

--- main.js
+++ main.js
@@ -3,72 +3,83 @@ console.log("Uddata++ starting");
 //Changes the current Uddata+ logo to the transparent version that allows the color of the navbar to be visible.
 $("#navbar>div>div>a>img").attr("src",chrome.extension.getURL("UddataLogo.png"));
 
+
+window.onload = function() {
+	setTimeout(function() {
+		$('.skemaBrikGruppe>g.GEIF5TWDNX>g>text>title').each(function(index) {
+			if ($(this).text().toUpperCase().includes("LEKTIE")) {
+				console.log("Thing");
+				$(this).parent().parent().parent().find('rect').css('fill-opacity', '0.0')
+			}
+		});
+	}, (8 * 1000));
+}
 curtheme = "Default";
 
 getStorage('theme', function (obj) {
-  if (!chrome.runtime.error) {
-    curtheme = obj.theme
-    runTheme();
-  }
+	if (!chrome.runtime.error) {
+		curtheme = obj.theme
+		runTheme();
+	}
 });
 
 
 //Changes color off element
 function runTheme(){
-  changeColor(colorElements.navBar, curtheme.navBar);
-  changeColor(colorElements.navbarIcon, curtheme.navbarIcon);
-  changeColor(colorElements.menuButtons, curtheme.navBar);
-  changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
-  changeColor(colorElements.menuFarve, curtheme.navBar);
+	changeColor(colorElements.navBar, curtheme.navBar);
+	changeColor(colorElements.navbarIcon, curtheme.navbarIcon);
+	changeColor(colorElements.menuButtons, curtheme.navBar);
+	changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
+	changeColor(colorElements.menuFarve, curtheme.navBar);
 }
 
 
 //Later changes
 function runThemeLater(){
 
-  changeColor(colorElements.pile, curtheme.navBar);
-  changeColor(colorElements.skemaButtons, curtheme.navBar);
+	changeColor(colorElements.pile, curtheme.navBar);
+	changeColor(colorElements.skemaButtons, curtheme.navBar);
 
-  changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
-  changeColor(colorElements.loginBtn, curtheme.navBar);
-  changeColor(colorElements.overSkrift, curtheme.navBar);
+	changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
+	changeColor(colorElements.loginBtn, curtheme.navBar);
+	changeColor(colorElements.overSkrift, curtheme.navBar);
 
 
 
-  changeColor(colorElements.skemaTop, curtheme.navBar);
-  changeColor(colorElements.arrows, curtheme.navBar);
-  changeColor(colorElements.tableButtons, curtheme.navBar);
-  changeColor(colorElements.tableTop, curtheme.navBar);
+	changeColor(colorElements.skemaTop, curtheme.navBar);
+	changeColor(colorElements.arrows, curtheme.navBar);
+	changeColor(colorElements.tableButtons, curtheme.navBar);
+	changeColor(colorElements.tableTop, curtheme.navBar);
 }
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.type == "theme"){
-            curtheme = request.theme
-            runTheme();
-            runThemeLater();
-        }
-    }
+	function(request, sender, sendResponse) {
+		if (request.type == "theme"){
+			curtheme = request.theme
+			runTheme();
+			runThemeLater();
+		}
+	}
 );
 
 getStorage('theme', function (obj) {
-  if (!chrome.runtime.error) {
-    curtheme = obj.theme
-    runTheme();
-  }
+	if (!chrome.runtime.error) {
+		curtheme = obj.theme
+		runTheme();
+	}
 });
 
 var checkExist = setInterval(function() {
-   if ($('h1').length) {
-      clearInterval(checkExist);
-      runThemeLater();
-   }
+	if ($('h1').length) {
+		clearInterval(checkExist);
+		runThemeLater();
+	}
 }, 1000);
 
 var checkExist = setInterval(function() {
-   if ($(".ace-nav>li.light-blue").css("background") == "#62a8d1") {
-      console.log("loool");
-      changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
-   }
+	if ($(".ace-nav>li.light-blue").css("background") == "#62a8d1") {
+		console.log("loool");
+		changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
+	}
 }, 1000);
 
