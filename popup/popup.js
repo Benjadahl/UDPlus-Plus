f084bf27a2ae8077a2dc3c7a29e49a8560d730f7
Rasmus Kjær <rasmase@gmail.com>
2016-08-25 18:11:14 +0200

    Fixed indent

--- popup/popup.js
+++ popup/popup.js
@@ -1,43 +1,43 @@
 var themes = {
-    "dark" : {"navBar": "#1d183d", "navbarIcon" : "#8f8f8f", "rightDropdown": "#171717"},
-    "green" : {"navBar": "#539e24", "navbarIcon" : "#ed8f12", "rightDropdown": "#1e4004"},
-    "red": {"navBar": "#ee3915", "navbarIcon": "#254918", "rightDropdown": "#e4642e"}}
+	"dark" : {"navBar": "#1d183d", "navbarIcon" : "#8f8f8f", "rightDropdown": "#171717"},
+	"green" : {"navBar": "#539e24", "navbarIcon" : "#ed8f12", "rightDropdown": "#1e4004"},
+	"red": {"navBar": "#ee3915", "navbarIcon": "#254918", "rightDropdown": "#e4642e"}}
 
-document.addEventListener('DOMContentLoaded', function() {
-  var themeSelect = document.getElementById('theme');
-	var fontSelect = document.getElementById('font');
+	document.addEventListener('DOMContentLoaded', function() {
+		var themeSelect = document.getElementById('theme');
+		var fontSelect = document.getElementById('font');
 
-	getStorage('theme', function (obj) {
-		if (!chrome.runtime.error) {
-      if(typeof obj.theme != "undefined"){
-			  themeSelect.value = obj.theme;
-      }else{
-        themeSelect.value = "default"
-      }
-		}
-	});
+		getStorage('theme', function (obj) {
+			if (!chrome.runtime.error) {
+				if(typeof obj.theme != "undefined"){
+					themeSelect.value = obj.theme;
+				}else{
+					themeSelect.value = "default"
+				}
+			}
+		});
 
-	getStorage('font', function (obj) {
-		if (!chrome.runtime.error) {
-      if(typeof obj.font != "undefined"){
-			  fontSelect.value = obj.font;
-      }else{
-        fontSelect.value = "Open sans"
-      }
-		}
-	});
+		getStorage('font', function (obj) {
+			if (!chrome.runtime.error) {
+				if(typeof obj.font != "undefined"){
+					fontSelect.value = obj.font;
+				}else{
+					fontSelect.value = "Open sans"
+				}
+			}
+		});
 
-	themeSelect.addEventListener('change', function() {
-		setStorage({'theme' : themes[theme.value]});
-		setStorage(themes[theme.value]);
-        //attempt to send message to content script
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            chrome.tabs.sendMessage(tabs[0].id, {type: "theme", theme: themes[theme.value]}, function(response) {});
-        });
+		themeSelect.addEventListener('change', function() {
+			setStorage({'theme' : themes[theme.value]});
+			setStorage(themes[theme.value]);
+			//attempt to send message to content script
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+				chrome.tabs.sendMessage(tabs[0].id, {type: "theme", theme: themes[theme.value]}, function(response) {});
+			});
+		}, false);
+
+		fontSelect.addEventListener('change', function() {
+			setStorage({'font' : fontSelect.value});
+		}, false);
+
 	}, false);
-
-	fontSelect.addEventListener('change', function() {
-		setStorage({'font' : fontSelect.value});
-	}, false);
-
-}, false);
