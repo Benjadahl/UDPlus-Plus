a2583a669ef5f851dc3c80d33c17c51b25b89c13
jbjjbjjbj <julianteule@gmail.com>
2016-08-25 14:22:33 +0200

    Made many more css changers, some not working

--- UD++Functions.js
+++ UD++Functions.js
@@ -1,8 +1,20 @@
 const colorElements =
-    {navBar:{cssSel:".navbar-inner", cssAttr:"background-color"},
-    rightDropdown: {cssSel:".ace-nav>li.light-blue", cssAttr:"background-color"},
-    navbarIcon:{cssSel:".ace-nav>li>a>[class*='icon-']", cssAttr:"color"}};
+    {navBar:{cssSel:".navbar-inner", cssAttr:["background-color"]},
+    rightDropdown: {cssSel:".ace-nav>li.light-blue", cssAttr:["background-color"]},
+    navbarIcon:{cssSel:".ace-nav>li>a>[class*='icon-']", cssAttr:["color"]},
+    skemaTop:{cssSel:".GEIF5TWDK- th.GEIF5TWDB-", cssAttr:["background-color", "border-top-color"]},
+    menuButtons:{cssSel:".nav-list>li.active>a, .nav-list>li.active>a:hover, .nav-list>li.active>a:focus, .nav-list>li.active>a:active", cssAttr:["color"]},
+    skemaButtons:{cssSel:".btn-info", cssAttr:["background-color","border-color"]},
+    pile:{cssSel:".nav-list li.active>a:after", cssAttr:["border-right-color"]},
+    overSkrift:{cssSel:"h1", cssAttr:["color"]}
+    };
 
 function changeColor (element, color) {
-    $(element.cssSel).css(element.cssAttr, color);
+
+    for(i = 0; i < element.cssAttr.length; i++){
+      $(element.cssSel ).each(function () { this.style.setProperty( element.cssAttr[i], color, 'important' ); });
+      //$(element.cssSel).css(element.cssAttr[i], color);
+      console.log(element.cssSel + "   " + element.cssAttr[i]);
+
+    }
 }
--- main.js
+++ main.js
@@ -3,13 +3,23 @@ console.log("Uddata++ starting");
 //Changes the current Uddata+ logo to the transparent version that allows the color of the navbar to be visible.
 $("#navbar>div>div>a>img").attr("src",chrome.extension.getURL("UddataLogo.png"));
 
+curtheme = "";
+
+function runTheme(){
+
+
+  changeColor(colorElements.navBar, curtheme.navBar);
+  changeColor(colorElements.rightDropdown, curtheme.rightDropdown);
+  changeColor(colorElements.navbarIcon, curtheme.navbarIcon);
+  changeColor(colorElements.menuButtons, curtheme.navBar);
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.type == "theme"){
             console.log(request.theme);
-            changeColor(colorElements.navBar, request.theme.navBar);
-            changeColor(colorElements.rightDropdown, request.theme.rightDropdown);
-            changeColor(colorElements.navbarIcon, request.theme.navbarIcon);
+            curtheme = request.theme
+            runTheme();
         }
     }
 );
@@ -17,8 +27,20 @@ chrome.runtime.onMessage.addListener(
 
 chrome.storage.sync.get('theme', function (obj) {
   if (!chrome.runtime.error) {
-    changeColor(colorElements.navBar, obj.theme.navBar);
-    changeColor(colorElements.rightDropdown, obj.theme.rightDropdown);
-    changeColor(colorElements.navbarIcon, obj.theme.navbarIcon);
+    curtheme = obj.theme
+    runTheme();
   }
 });
+
+var checkExist = setInterval(function() {
+   if ($('h1').length) {
+      clearInterval(checkExist);
+      console.log("Lol");
+
+      changeColor(colorElements.pile, curtheme.navBar);
+      changeColor(colorElements.skemaButtons, curtheme.navBar);
+      changeColor(colorElements.skemaTop, curtheme.navBar);
+      //changeColor(colorElements.overskrift, curtheme.navbarIcon);
+
+   }
+}, 1000);
