3a400cd74046b1262aebd723b2863b362adce3f7
Julian <julianteule@gmail.com>
2016-09-06 19:01:29 +0200

    Converted red thtme to new format

--- themes.js
+++ themes.js
@@ -4,7 +4,6 @@ var themes = {
         "name": "default"
      },
 	"black" : {"name": "black", "navbar": "rgb(43, 43, 43)", "navbarIcon" : "#FFFFFF", "rightDropdown": "rgb(43, 43, 43)"},
-	"red": {"name": "red", "navbar": "#B22222", "navbarIcon": "#FFFF99", "rightDropdown": "rgba(0, 0, 0, 0.2)"},
 	"blue": {"name": "blue", "navbar": "#0375B4", "navbarIcon": "#FFFFFF", "rightDropdown": "rgba(0, 0, 0, 0.2)"},
 	"green" : {
 			"name": "green",
@@ -12,8 +11,9 @@ var themes = {
 			"navbarIcon" : "#FFFFFF",
 			"rightDropdown": "rgb(53, 115, 6)",
 			"rightDropdownRightEdge": "#539e24", 
-			"profileRing": "#539e24",
+			"profileRing": "rgb(53, 115, 6)",
 			"studentInfo":"#999999",
+			"header":"#539e24",
 			"tableButtons":"#539e24",
 			"loginBtn":"#539e24",
 			"tableTopActive":"#539e24",
@@ -24,6 +24,27 @@ var themes = {
 			"linkLanguage":"#539e24",
 			"mainText":"#539e24",
 			"leftMenuArrowBorder":"#539e24",
+			"lessonOpacity":"1"
+	},
+	"red" : {
+			"name": "red",
+			"navbar": "#B22222",
+			"navbarIcon" : "#FFFF99",
+			"rightDropdown": "rgba(0, 0, 0, 0.2)",
+			"rightDropdownRightEdge": "#B22222", 
+			"profileRing": "#B22222",
+			"studentInfo":"#999999",
+			"header":"#B22222",
+			"tableButtons":"#B22222",
+			"loginBtn":"#B22222",
+			"tableTopActive":"#B22222",
+			"lessonFill":"#B22222",
+			"lessonStroke":"#B22222",
+			"arrows":"#B22222",
+			"leftMenuTextActive":"#B22222",
+			"linkLanguage":"#B22222",
+			"mainText":"#B22222",
+			"leftMenuArrowBorder":"#B22222",
 			"lessonOpacity":"1"
 	},
 		"dark" : {
