23e864d6248a944108a64e694d725e683e52c977
benjadahl <benjadahl@gmail.com>
2016-10-09 01:34:59 +0200

    Maded an example that shows the cross-origin error

--- background/fetchAvatars.js
+++ background/fetchAvatars.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-  console.log($("#frame").contents().find("div").html());
+  console.log($("#frame").contents().find("a").attr("href"));
   console.log($("#frame").attr("src"));
 });
 
-
+setTimeout(function() {
+  console.log($("#frame").contents().find("a").attr("href"));
+}, 5000);
 console.log("test");
