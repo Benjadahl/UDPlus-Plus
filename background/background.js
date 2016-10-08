97439b608b089309d9edb61f66ecf64f6b3bf36b
benjadahl <benjadahl@gmail.com>
2016-10-08 22:37:31 +0200

    Made the basics for a fetching system

--- background/background.js
+++ background/background.js
@@ -1,3 +1,4 @@
+//Listener for the button press from the ++settings button
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   if(message.optionsClick){
     chrome.runtime.openOptionsPage();
---
+++ background/fetchAvatars.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  console.log($("#frame").contents().find("div").html());
+  console.log($("#frame").attr("src"));
+});
+
+
+console.log("test");
