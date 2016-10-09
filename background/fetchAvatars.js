f676ffa12f1eaebd40f6590ef7a873283751655f
benjadahl <benjadahl@gmail.com>
2016-10-09 22:48:52 +0200

    Made a way to fetch the page without cross origin errors

--- background/fetchAvatars.js
+++ background/fetchAvatars.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
 });
 
 setTimeout(function() {
-  console.log($("#frame").contents().find("a").attr("href"));
+  //console.log($("#frame").contents().find("a").attr("href"));
+  //console.log($("#frame").html());
+  //console.log(document.getElementById("frame").contentDocument);
 }, 5000);
 console.log("test");
+var xhr = new XMLHttpRequest();
+xhr.open("GET","https://www.uddataplus.dk/besked/",false);
+xhr.send();
+
+var result = xhr.responseText;
+
+
+
+setInterval(function() {
+  console.log(result);
+}, 1000);
