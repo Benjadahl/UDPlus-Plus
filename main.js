f853fa315c8c89e2b91074a82f7276eb365453bb
Rasmus Rendal <rasmusrendal@gmail.com>
2016-10-12 13:50:02 +0200

    Revert "Fixed dynamically changing themes"
    This reverts commit 34c1be43ca0c25bf25af21be4c949e981e3401ca.

--- main.js
+++ main.js
@@ -3,10 +3,25 @@ console.log("Uddata++ starting");
 //Changes the current Uddata+ logo to the transparent version that allows the color of the navbar to be visible.
 $("#navbar>div>div>a>img").attr("src",chrome.extension.getURL("resources/UddataLogo.png"));
 
-//Define the variable curtheme to contain the current theme
-var curtheme = "Default";
+//Save the language selected on Uddata+
+if($("#language > a").html() == "English"){
+	setStorage({"lang": "dansk"});
+}else{
+	setStorage({"lang": "engelsk"});
+}
 
 var homeworkList = ["lektie"];
+getStorage({homeworkWords: "lektie,forbered"}, function(obj) {
+	if (!chrome.runtime.error) {
+		homeworkList = obj.homeworkWords.split(",");
+		//We have to remove the empty elements, or everything will be matched as homework.
+		for (var i=0; i < homeworkList.length; i++) {
+			homeworkList[i] = homeworkList[i].replace(/\s/g, "");
+			if (homeworkList[i] == "") homeworkList.splice(i, 1);
+		}
+		if (homeworkList == [""]) homeworkList.splice(0, 1);
+	}
+});
 
 // <---- HOMEWORK MARKING
 //Function for marking the homework
@@ -28,75 +43,50 @@ function markHomework(){
 	});
 }
 
-//We need to use this function to load all the settings
-function loadSettings() {
-
-	//Keywords for checking homework
-	getStorage({homeworkWords: "lektie,forbered"}, function(obj) {
-		if (!chrome.runtime.error) {
-			homeworkList = obj.homeworkWords.split(",");
-			//We have to remove the empty elements, or everything will be matched as homework.
-			for (var i=0; i < homeworkList.length; i++) {
-				homeworkList[i] = homeworkList[i].replace(/\s/g, "");
-				if (homeworkList[i] == "") homeworkList.splice(i, 1);
-			}
-			if (homeworkList == [""]) homeworkList.splice(0, 1);
-		}
+//On the download on class notes, we set the title attribute to the download attribute. Then, if the full title ends up in the overflow, you can mouse over it to see it anyway.
+function setTitleToDownload() {
+	$( "a[download]" ).each(function( index ) {
+  	$(this).attr("title", $(this).attr("download"));
 	});
+}
+setInterval(setTitleToDownload, 250);
 
+//Check if we are actually on the schedule page
+if(window.location.href.includes("skema")){
 	//Get the homework setting
-	var homeworkCheckerInterval;
 	getStorage('homework', function (obj) {
 		if (!chrome.runtime.error) {
 			//If the schedule object exists and the homework setting is true, setup interval to mark
 			if (window.location.href.indexOf("skema")) {
 				if(obj.homework){
 					//Interval to mark homework, they will be marked when they load in
-					clearInterval(homeworkCheckerInterval);
-					homeworkCheckerInterval = setInterval(function() {
+					var homeWorkInterval = setInterval(function() {
+						console.log("looping");
 						markHomework();
+						//Clear the interval when the homework description exists, to save up resources
+						//if($(".skemaBrikGruppe>g>g>text>title").length > 0){
+						//	clearInterval(homeWorkInterval);
+						//}
 					}, 250);
 				}
 			}
 		}
 	});
-
-	getStorage('theme', function (obj) {
-		if (!chrome.runtime.error) {
-			curtheme = obj.theme;
-			runTheme();
-		}
-	});
-
 }
 
-//Save the language selected on Uddata+
-if($("#language > a").html() == "English"){
-	setStorage({"lang": "dansk"});
-}else{
-	setStorage({"lang": "engelsk"});
-}
+//Define the variable curtheme to contain the current theme
+var curtheme = "Default";
 
-
-//On the download on class notes, we set the title attribute to the download attribute. Then, if the full title ends up in the overflow, you can mouse over it to see it anyway.
-function setTitleToDownload() {
-	$( "a[download]" ).each(function( index ) {
-		$(this).attr("title", $(this).attr("download"));
-	});
-}
-setInterval(setTitleToDownload, 250);
-
-loadSettings();
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-	//Try to import the theme from the settings storage
-	loadSettings();
+//Try to import the theme from the settings storage
+getStorage('theme', function (obj) {
+	if (!chrome.runtime.error) {
+		curtheme = obj.theme;
+		runTheme();
+	}
 });
-
 
 //Changes color off each element in the current theme
 function runTheme(){
-	$('.UDPPCustom').remove();
 	for (var T in themes[curtheme]) {
 		if(T != "homeworkMark"){
 			changeColor(colorElements[T], themes[curtheme][T]);
@@ -118,13 +108,13 @@ $(document).ready(function(){
 
 //Wait for change in theme from popup
 chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-			if (request.type == "theme"){
-				curtheme = request.theme;
-				location.reload();
-			}
+	function(request, sender, sendResponse) {
+		if (request.type == "theme"){
+			curtheme = request.theme;
+			location.reload();
 		}
-		);
+	}
+);
 
 //Get current theme from settings and execute the function that switches theme
 getStorage('theme', function (obj) {
