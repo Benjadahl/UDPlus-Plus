fa11b9b9533cbdc865da339ef1161975d1d98fe7
benjadahl <benjadahl@gmail.com>
2016-10-13 15:23:55 +0200

    Put custom theme files into its own folder

--- options/options.js
+++ options/options.js
@@ -35,7 +35,6 @@ for (var key in themes) {
 getStorage('theme', function (obj) {
 	if (!chrome.runtime.error) {
 		if (typeof obj.theme != "undefined"){
-			console.log(obj.theme);
 			$('#theme').val(obj.theme);
 		} else {
 			$('#theme').val("default");
