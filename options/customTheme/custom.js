6fad38f4c1036192eba9fd22146bba94749ad0fb
Julian <julianteule@gmail.com>
2016-10-18 10:24:50 +0200

    Added slider for transparensy

--- options/customTheme/custom.js
+++ options/customTheme/custom.js
@@ -12,8 +12,11 @@ getStorage('lang', function (obj) {
 
 //We start out by making the textboxes for the different colors to change
 for(var T in customTemplate){
-    //$("#frame").append(T + ' : <input type="text" id="' + T + '"></input><br><br>');
-    $("#frame").append(T + ' : <input type="text" class="jscolor {required:false, hash:true}" id="' + T + '"></input><br><br>');
+    if(T != "Lesson_Transparent"){
+        $("#frame").append(T + ' : <input type="text" class="jscolor {required:false, hash:true}" id="' + T + '"></input><br><br>');
+    }else{
+        $("#frame").append(T + ' | Fra 0 til 1 : <input type="range" min="0" max="1" step="0.05" id="' + T + '" ></input><br><br>');
+    }
 }
 
 //This function is run then the user selects a new theme
