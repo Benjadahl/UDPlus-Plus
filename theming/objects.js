a4a64ec4c4bf96b452b7b7ac91084b82d2edacd3
jbjjbjjbj <julianteule@gmail.com>
2016-12-04 12:50:44 +0100

    Added alot of selectors to new selector method

--- theming/objects.js
+++ theming/objects.js
@@ -4,14 +4,24 @@ var PlusPlusList = {
     rightDropdown:new PlusPlusObject(".ace-nav>li.light-blue", applyCssRule, ["background-color"]),
     navbarIcon:new PlusPlusObject(".ace-nav>li>a>[class*='icon-']", applyCssRule, ["color"]),
     tableTopActive:new PlusPlusObject(".GNK2GVDDN- th.GNK2GVDDE-", applyCssRule, ["background-color", "border-top-color"]),
-    leftMenuLIborderBottom:new PlusPlusObject(".nav-list>li", applyBorderRule, ["border-bottom"], flags={pixels: 1})  
+    leftMenuLIborderBottom:new PlusPlusObject(".nav-list>li", applyBorderRule, ["border-bottom"], flags={pixels: 1}),
+    popOutMenuText: new PlusPlusObject(".menu-text", applyCssRule, ["background-color"]),
+    sidebarColor: new PlusPlusObject(".sidebar", applyCssRule, ["background-color", "border-right"]),
+    mainContainerH: new PlusPlusObject(".main-container", applyCssRule, ["height"])
 
   },
   start:{},
-  schedule:{},
+  schedule:{
+    scheduleTopAnnouncements: new PlusPlusObject(".GNK2GVDDPHB", applyCssRule, ["background-color"])
+  },
   absence:{},
   calender:{},
-  conversations:{},
+  conversations:{
+    commentMessageText: new PlusPlusObject(".K1CYATD-u-f", applyCssRule, ["color"]),
+    commentWritingBackground: new PlusPlusObject(".gwt-TextArea", applyCssRule, ["background-color"]),
+    activeMessage: new PlusPlusObject(".K1CYATD-j-e", applyCssRule, ["background-color"]),
+    messageBackground: new PlusPlusObject(".K1CYATD-u-f", applyCssRule, ["background-color"])
+  },
   plan:{},
   assignemnets:{},
   resources:{},
