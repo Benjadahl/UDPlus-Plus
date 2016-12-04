92bf86790f82cf8cae1c60870fc4889199520ebf
benjadahl <benjadahl@gmail.com>
2016-12-04 13:04:24 +0100

    Added more selectors to the new list

--- theming/objects.js
+++ theming/objects.js
@@ -4,16 +4,28 @@ var PlusPlusList = {
     rightDropdown:new PlusPlusObject(".ace-nav>li.light-blue", applyCssRule, ["background-color"]),
     navbarIcon:new PlusPlusObject(".ace-nav>li>a>[class*='icon-']", applyCssRule, ["color"]),
     tableTopActive:new PlusPlusObject(".GNK2GVDDN- th.GNK2GVDDE-", applyCssRule, ["background-color", "border-top-color"]),
-    leftMenuLIborderBottom:new PlusPlusObject(".nav-list>li", applyBorderRule, ["border-bottom"], flags={pixels: 1})  
-
+    leftMenuLIborderBottom:new PlusPlusObject(".nav-list>li", applyBorderRule, ["border-bottom"], flags={pixels: 1}),
+    loginBtn:new PlusPlusObject(".btn", applyCssRule, ["background-color", "border-color"]),
+    navbarEdge:new PlusPlusObject("#menuFarve", applyCssRule, ["background-color"]),
+    backEdge:new PlusPlusObject("body", applyCssRule, ["background-color"]),
+    tableButtons:new PlusPlusObject(".btn-info", applyCssRule, ["background-color","border-color"]),
+    arrows:new PlusPlusObject(".nav-list li.active>a:after", applyCssRule, ["border-right-color"]),
+    header:new PlusPlusObject("h1", applyCssRule, ["color"]),
+    mainBackground:new PlusPlusObject(".page-content", applyCssRule, ["background"]),
+    leftMenuBottom:new PlusPlusObject(".sidebar:before", applyCssRule, ["background-color"]),
+    leftMenuBorder:new PlusPlusObject(".sidebar:before", applyBorderRule, ["border-right"])
+    
   },
   start:{},
-  schedule:{},
-  absence:{},
+  schedule:{
+    
+  },
+  absence:{
+  },
   calender:{},
   conversations:{},
   plan:{},
   assignemnets:{},
   resources:{},
   grades:{}
-}
+};
