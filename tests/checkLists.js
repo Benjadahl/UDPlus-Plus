deda786ddfe4f3ffcf08e992295258570acb1100
Rasmus Kjær <rasmusrendal@gmail.com>
2016-12-18 12:23:25 +0100

    Added some documentation

--- tests/checkLists.js
+++ tests/checkLists.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	//We basically go through the colorElements, and then check if all the objects there also have something in the new system
 	for (var style in colorElements) {
 		var isIn = false;
 		for (var category in PlusPlusList) {
@@ -6,6 +7,7 @@ window.onload = function() {
 				if (check == style) isIn = true;
 			}
 		}
+		//We add it to an UL on the page
 		if (!isIn) document.getElementById("missing").innerHTML += "<li>" + style + "</li>";
 	}
 };
