32eb05954afa9d2e578f45040d87b15eda75c848
Rasmus Rendal <rasmusrendal@gmail.com>
2016-12-18 17:29:32 +0100

    Further splitted up main.js

--- UD++Functions.js
+++ UD++Functions.js
@@ -97,3 +97,14 @@ function changeColor (element, color) {
 		}
 	}
 }
+
+function stringToList(string) {
+	var thelist = string.split(",");
+	for (var i=0; i<thelist.length; i++) {
+		thelist[i] = thelist[i].replace(/\s/g, "");
+		if (thelist[i] == "") thelist.splice(i,1);
+	}
+	if (thelist === [""]) thelist.splice(0,1);
+	return thelist;
+}
+
