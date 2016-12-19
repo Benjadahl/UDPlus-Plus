8882e6a7d22fb2295099266a9e0dbe4d11fef0ff
Rasmus Rendal <rasmusrendal@gmail.com>
2016-12-19 21:53:46 +0100

    Laid groundwork for background easy-a check

--- background.js
+++ background.js
@@ -1,13 +1,99 @@
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-  if(message.optionsClick){
-    chrome.runtime.openOptionsPage();
-  }
+	if(message.optionsClick){
+		chrome.runtime.openOptionsPage();
+	}
 });
 
 chrome.runtime.onInstalled.addListener(function(details){
-  if(details.reason === "update"){
-    //This code will run every time the plugin is updated
-    //It will make the news paragraph appear under the ++ Settings button
-    setStorage({'showNews' : true});
-  }
+	if(details.reason === "update"){
+		//This code will run every time the plugin is updated
+		//It will make the news paragraph appear under the ++ Settings button
+		setStorage({'showNews' : true});
+	}
 });
+
+//A regular expression which parses a title from the RSS feed, and extracts the good info.
+var downRegex = /EASY-A lukker ned (.*) den (\d\d)\/(\d\d) kl\. ((\d\d:\d\d) - (\d\d:\d\d)|(\d\d)-(\d\d))/;
+
+//This function will check EASY-A for downtime
+function checkEasyADowntime() {
+	var currentDate = new Date();
+	//EASY-A's update site
+	var url = 'http://admsys.stil.dk/Service/RSS/RSS/EASY-A-Nyhedsliste.rss';
+
+	$.get(url, function (data) {
+		$(data).find("channel > item").each(function() {
+			//The title of the entry
+			var title = $(this).find("title").html();
+			//The date that the RSS feed entry was published
+			var pubDate = $(this).find("pubDate").html();
+
+			var regexMatch = title.match(downRegex);
+
+			if (regexMatch !== null) {
+				console.log(regexMatch);
+
+				//A date object is easier to work with
+				var pubDate = new Date(Date.parse(pubDate));
+
+				var year = "/" + (1900 + pubDate.getYear());
+				//This one returns true if pubDate already passed the month EASY-A will go down.
+				if (regexMatch[3] < pubDate.getMonth() + 1) {
+					var year = 1901 + pubDate.getYear();
+					var year = "/" + year;
+				}
+
+				//Now we know the date that the site is going down
+				var downDate = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year);
+
+				var downStartTime, downEndTime;
+				//Check if the time has minutes
+				if (regexMatch[4].length > 5) {
+					downStartTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[5]);
+					downEndTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[6]);
+					if (downStartTime > downEndTime) {
+						downEndTime = Date.parse(regexMatch[3] + "/" + ((parseInt(regexMatch[2]) + 1)) + year + " " + regexMatch[6]);
+					}
+				} else {
+					downStartTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[7] + ":00");
+					downEndTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[8] + ":00");
+					if (downStartTime > downEndTime) {
+						downEndTime = Date.parse(regexMatch[3] + "/" + ((parseInt(regexMatch[2]) + 1)) + year + " " + regexMatch[8]) + ":00";
+					}
+				}
+				//After that huge mess, we now got the start and end times
+				var url = $(this).find("link").html();
+
+				if (downEndTime > currentDate) {
+					console.log("Going down");
+
+					//Proper date elements format nicer than a big ol' timestamp
+					downStartTime = new Date(downStartTime);
+					downEndTime = new Date(downEndTime);
+
+					//Let's create a notification! This is mostly copy-pasted, so change it as much as you'd like.
+					chrome.notifications.create({
+						iconUrl: chrome.runtime.getURL('resources/icons/icon48.png'),
+						title: 'Uddata going down',
+						type: 'basic',
+						message: 'UDDATA is going to be down from ' + downStartTime + " to " + downEndTime + ".",
+						buttons: [{ title: 'Learn More' }],
+						isClickable: true,
+						priority: 2,
+					}, function() { });
+
+					chrome.notifications.onButtonClicked.addListener(function() {
+						chrome.tabs.create({
+							url: url
+						});
+					});
+				}
+			}
+		})
+	});
+}
+
+checkEasyADowntime();
+
+setInterval(checkEasyADowntime, 1000 * 60 * 20);
+//Check EASY-A for new downtime info every 20 minutes.
