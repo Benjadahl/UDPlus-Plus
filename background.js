1c7797b74c09c3bdf03358549b31bd13d379702b
Rasmus Rendal <rasmusrendal@gmail.com>
2016-12-20 14:56:01 +0100

    Added comments, and fixed a little bug

--- background.js
+++ background.js
@@ -29,6 +29,7 @@ var weekDays = {
 function checkEasyADowntime() {
 	var currentDate = new Date();
 
+	//We use this to check if anything is going down now. If not, we change the message setting to blank.
 	var isGoingDown = false;
 
 	//Uncomment this for testing. This is a timestamp where the downtime would be relevant.
@@ -38,6 +39,7 @@ function checkEasyADowntime() {
 	//EASY-A's update site
 	var url = 'http://admsys.stil.dk/Service/RSS/RSS/EASY-A-Nyhedsliste.rss';
 
+	//jQuery GET request
 	$.get(url, function (data) {
 		$(data).find("channel > item").each(function() {
 			//The title of the entry
@@ -46,6 +48,20 @@ function checkEasyADowntime() {
 			var pubDate = $(this).find("pubDate").html();
 
 			var regexMatch = title.match(downRegex);
+			/*
+			 * This returns a bit complicated stuff, so I'll explain the whole array
+			 * 0: The whole title
+			 * 1: Day of the week in Danish
+			 * 2: The day of the month
+			 * 3: The month of the year in Danish
+			 * 4: The whole "time" part of the title, as in when it'll be down
+			 * 5: In the case of time with minutes, this is the start time
+			 * 6: In the case of time with minutes, this is the end time
+			 * 7: In the case of time without minutes, this is the start time
+			 * 9: In the case of time without minutes, this is the end time.
+			 */
+
+			console.log(regexMatch);
 
 			if (regexMatch !== null) {
 				console.log(regexMatch);
@@ -64,7 +80,7 @@ function checkEasyADowntime() {
 				var downDate = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year);
 
 				var downStartTime, downEndTime;
-				//Check if the time has minutes
+				//Check if the time has minutes, and then format the start and end times into a javascript Date object depending on if it has
 				if (regexMatch[4].length > 5) {
 					downStartTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[5]);
 					downEndTime = Date.parse(regexMatch[3] + "/" + regexMatch[2] + year + " " + regexMatch[6]);
@@ -79,6 +95,8 @@ function checkEasyADowntime() {
 					}
 				}
 				//After that huge mess, we now got the start and end times
+
+				//This is the URL to the article. We can use this.
 				var url = $(this).find("link").html();
 
 				//If it's not already done going down.
@@ -106,6 +124,7 @@ function checkEasyADowntime() {
 				}
 			}
 		});
+		//Instead of timing it properly, we just wait 100ms and hope it'll all be over then. It probably is, so it's no big problem
 		window.setTimeout(function() {
 			if (!isGoingDown) setStorage({"message": '' });
 		}, 100);
@@ -118,9 +137,8 @@ function sendDownMessage(message, href) {
 	getStorage('message', function(obj) {
 		if (!chrome.runtime.error) {
 			if (link !== obj.message) {
-				console.log(link);
-				console.log(obj.message);
-				//Let's create a notification! This is mostly copy-pasted, so change it as much as you'd like.
+
+				//Creates a chrome notification. Mostly copy-pasted, should probably be changed
 				chrome.notifications.create({
 					iconUrl: chrome.runtime.getURL('resources/icons/icon48.png'),
 					title: 'Uddata going down',
@@ -131,9 +149,10 @@ function sendDownMessage(message, href) {
 					priority: 2,
 				}, function() { });
 
+				//Adds a listener to the notification that opens the url when we click it.
 				chrome.notifications.onButtonClicked.addListener(function() {
 					chrome.tabs.create({
-						url: url
+						url: href
 					});
 				});
 
@@ -145,5 +164,5 @@ function sendDownMessage(message, href) {
 
 checkEasyADowntime();
 
-setInterval(checkEasyADowntime, 1000 * 60 * 20);
 //Check EASY-A for new downtime info every 20 minutes.
+setInterval(checkEasyADowntime, 1000 * 60 * 20);
