1052c0c6d1511c039b1bf2470e93f8af1160beaa
benjadahl <benjadahl@gmail.com>
2016-12-20 16:56:45 +0100

    Revert "Set up new structure"
    This reverts commit 955860262de01ad131876ac393a202b9d0d4ba1d.

--- src/pageScripts/message.js
+++
@@ -1,9 +0,0 @@
-//Set the current page variable
-curPage = "conversations";
-
-var messageFix = setInterval(function(){
-	if($(".gwt-TextArea").length > 0){
-		//$(".gwt-TextArea").css("background-color","rgba(176, 5, 60, 0)");
-    document.getElementsByClassName("gwt-TextArea")[0].style.color = "rgba(176, 5, 60, 0)";
-	}
-}, 250);
