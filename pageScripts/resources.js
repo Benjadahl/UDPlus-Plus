f368a67286d52c7a72eaa83aaca36e7b5003736d
benjadahl <benjadahl@gmail.com>
2016-12-20 17:04:46 +0100

    Added the deleted files

--- pageScripts/resources.js
+++
@@ -1,7 +0,0 @@
-//Set the current page variable
-curPage = "resources";
-
-var checkExist = setInterval(function() {
-   if ($("h1").length) {
-     $(".MTWCGV-c-g").css("max-height", "400px");
-}});
