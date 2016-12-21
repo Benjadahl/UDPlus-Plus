5d7490538342ca745a1d1c17fba72fa38649d12b
Benjadahl <benjadahl@gmail.com>
2016-12-21 16:10:59 +0100

    Added comments to test files

--- src/tests/testFunctions.js
+++ src/tests/testFunctions.js
@@ -1,6 +1,11 @@
+//This function tests wheter the stringToList() function is working properly
 function testStringToList(){
+  //Create the array with the function result
   var test = stringToList("Lektier, homework, ,read");
-  if(test[0] === "Lektier" && test[1] === "homework" && test[2] === "read"){
+  //Check for the expected results
+  if (test[0] === "Lektier" && test[1] === "homework" && test[2] === "read") {
     return true;
+  } else {
+    return false;
   }
 }
--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,6 +1,7 @@
+//tests object that all test functions should be added to
 var tests = {stringToList: testStringToList()};
 
-
+//Append all element of tests to the list, and add their result
 for (var test in tests){
   $("#testList").append("<li>" + test + ": " + tests[test] + "</li>");
 }
