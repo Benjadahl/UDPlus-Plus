38f3cb81a520fd7f3a6a203cf0d22a49a536c1d9
Rasmus Rendal <rasmusrendal@gmail.com>
2017-01-10 09:34:06 +0100

    Only show cached schedule if it's for the current week.

--- pageScripts/uddata-down.js
+++ pageScripts/uddata-down.js
@@ -1,3 +1,10 @@
+Date.prototype.getWeekNumber = function(){
+	var d = new Date(+this);
+	d.setHours(0,0,0,0);
+	d.setDate(d.getDate()+4-(d.getDay()||7));
+	return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
+
 window.onload = function() {
 	getStorage("cachedSchedule", true, function(obj) {
 		if (!chrome.runtime.error) {
@@ -8,16 +15,21 @@ window.onload = function() {
 					var lang = obj.lang;
 					getStorage("cachedScheduleDate", true, function(obj) {
 						var date = obj.cachedScheduleDate;
-						console.log(obj);
-						$("body").append('<script src="https://use.fontawesome.com/5b06aadc00.js"></script>');
-						if (lang == "dansk") {
-							$("body").append("<center><h2>UDDATA+ er nede, men UD++ har cached dit skema for dig. (Hentet: " + date + ")</h2></center>");
-							$("body").append("<center>Hold musen over <p style='font-family: FontAwesome; display: inline;'></p> ikonet for at vise noter på lektionerne.</center>");
-						} else {
-							$("body").append("<center><h2>UDDATA+ is down, but UD++ has cached your schedule for you. (Retrieved: " + date + ")</h2></center>");
-							$("body").append("<center>Hover the cursor above the <p style='font-family: FontAwesome; display: inline;'></p> icon to show the notes on the lessons.</center>");
+						var currentWeekNumber = new Date().getWeekNumber();
+						var scheduleWeekNumber = new Date(date).getWeekNumber();
+						if (scheduleWeekNumber === currentWeekNumber) {
+							console.log(obj);
+							$("body").append('<script src="https://use.fontawesome.com/5b06aadc00.js"></script>');
+							console.log(new Date(date).getWeekNumber());
+							if (lang == "dansk") {
+								$("body").append("<center><h2>UDDATA+ er nede, men UD++ har cached dit skema for dig. (Hentet: " + date + ")</h2></center>");
+								$("body").append("<center>Hold musen over <p style='font-family: FontAwesome; display: inline;'></p> ikonet for at vise noter på lektionerne.</center>");
+							} else {
+								$("body").append("<center><h2>UDDATA+ is down, but UD++ has cached your schedule for you. (Retrieved: " + date + ")</h2></center>");
+								$("body").append("<center>Hover the cursor above the <p style='font-family: FontAwesome; display: inline;'></p> icon to show the notes on the lessons.</center>");
+							}
+							$("body").append("<center>" + schedule + "</center>");
 						}
-						$("body").append("<center>" + schedule + "</center>");
 
 					});
 				});
