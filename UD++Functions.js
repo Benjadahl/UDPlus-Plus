1511a9d0c433f47ad100a12de873561f244e748c
benjadahl <benjadahl@gmail.com>
2017-01-11 18:18:51 +0100

    Cleaned repository for github pages

--- UD++Functions.js
+++
@@ -1,9 +0,0 @@
-function stringToList(string) {
-	var thelist = string.split(",");
-	for (var i=0; i<thelist.length; i++) {
-		thelist[i] = thelist[i].replace(/\s/g, "");
-		if (thelist[i] == "") thelist.splice(i,1);
-	}
-	if (thelist === [""]) thelist.splice(0,1);
-	return thelist;
-}
--- tests/showCachedSchedule.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-	getStorage('cachedSchedule', true, function(obj) {
-		if (!chrome.runtime.error) {
-			document.getElementById("schedule").innerHTML = cachedSchedule;
-		}
-	});
-}
