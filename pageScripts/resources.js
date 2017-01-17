a4ffb138a291266668a137e99389172f4bfe3784
Rasmus Rendal <rasmusrendal@gmail.com>
2017-01-17 13:57:22 +0100

    Fixed title too

--- pageScripts/resources.js
+++ pageScripts/resources.js
@@ -1,8 +1,11 @@
 //Set the current page variable
 curPage = "resources";
 
+
 var checkExist = setInterval(function() {
-   if ($("h1").length) {
-     $(".MTWCGV-c-g").css("max-height", "400px");
-		 $(".page-header > h1:contains('Ressources')").html("Resources");
-}});
+	if ($("h1").length) {
+		$(".MTWCGV-c-g").css("max-height", "400px");
+		$(".page-header > h1:contains('Ressources')").html("Resources");
+		if (document.title == "Ressources") document.title = "Resources";
+	}
+});
