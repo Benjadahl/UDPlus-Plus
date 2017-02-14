ff6fd4010363edcb952f71f2f55cb36dd0bc908a
Benjadahl <benjadahl@gmail.com>
2017-02-14 20:49:46 +0100

    Added calender config from demo

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -56,17 +56,77 @@ window.onload = function() {
 
 
 	$('#calendar').fullCalendar({
-		selectable: false,
 		defaultView: "agendaWeek",
-		editable: false,
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay,listWeek',
+		},
+		defaultDate: '2016-12-12',
+		navLinks: true, // can click day/week names to navigate views
+
+		weekNumbers: true,
+		weekNumbersWithinDays: true,
+		weekNumberCalculation: 'ISO',
+
+		editable: true,
+		eventLimit: true, // allow "more" link when too many events
 		events: [
 			{
-				title: "Test",
-				start: new Date(2017, 02, 15, 15, 0, 0, 0),
-				end: new Date(2017, 02, 15, 16, 0, 0, 0)
+				title: 'All Day Event',
+				start: '2016-12-01'
+			},
+			{
+				title: 'Long Event',
+				start: '2016-12-07',
+				end: '2016-12-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2016-12-09T16:00:00'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2016-12-16T16:00:00'
+			},
+			{
+				title: 'Conference',
+				start: '2016-12-11',
+				end: '2016-12-13'
+			},
+			{
+				title: 'Meeting',
+				start: '2016-12-12T10:30:00',
+				end: '2016-12-12T12:30:00'
+			},
+			{
+				title: 'Lunch',
+				start: '2016-12-12T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: '2016-12-12T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: '2016-12-12T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: '2016-12-12T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: '2016-12-13T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2016-12-28'
 			}
 		]
-		//Callbacks?
 	});
 }
 
