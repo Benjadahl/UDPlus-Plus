fa3827f00e064c737c3638029726b77a8cca5ef5
Rasmus Rendal <rasmusrendal@gmail.com>
2017-04-18 12:50:02 +0200

    Autofetching classes instead of having a human do it

--- pageScripts/conversations.js
+++ pageScripts/conversations.js
@@ -8,4 +8,18 @@ window.onload = function() {
 			$(this).val(newVal);
 		});
 	}, 2000);
+	checkForSelectors();
+
 }
+
+function checkForSelectors() {
+	var classes = $("div>span.gwt-InlineHTML:nth-child(2)").attr("class");
+	if (typeof classes !== 'undefined') {
+		var selector = classes.split(" ")[1];
+		setStorage({'commentTextSelector': "." + selector});
+		console.log(selector);
+	} else {
+		window.setTimeout(checkForSelectors, 1000);
+	}
+}
+
