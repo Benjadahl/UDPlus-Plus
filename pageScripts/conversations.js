acf949ff0b22a5e075b74e38688d0159ff02c3e6
Rasmus Rendal <rasmusrendal@gmail.com>
2017-05-05 18:10:50 +0200

    Fixed scrolling on conversations

--- pageScripts/conversations.js
+++ pageScripts/conversations.js
@@ -12,11 +12,27 @@ window.onload = function() {
 
 }
 
+var lastPos = 0;
+function scrollEvent(e) {
+	var element = $(".always-visible.ps-container.ps-active-y");
+	var height = element.prop('scrollHeight') - element.innerHeight();
+	var newPos = element.scrollTop();
+	if (newPos == height) {
+		if (newPos - lastPos > 60) {
+			element.scrollTop(lastPos);
+			console.log("Fixed scroll height;");
+		}
+	} else {
+		lastPos = newPos;
+	}
+}
+
 function checkForSelectors() {
 	var classes = $("div>span.gwt-InlineHTML:nth-child(2)").attr("class");
 	if (typeof classes !== 'undefined') {
 		var selector = classes.split(" ")[1];
 		setStorage({'commentTextSelector': "." + selector});
+		$(".always-visible.ps-container.ps-active-y").scroll(scrollEvent);
 	} else {
 		window.setTimeout(checkForSelectors, 1000);
 	}
