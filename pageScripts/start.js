2ecb9f85f9566fd39fc514de2932cf4ef16ed017
Julian Teu <julianteule@gmail.com>
2017-09-27 19:38:03 +0200

    Check every second if 'you do not have access' popup exist, and click OK

--- pageScripts/start.js
+++ pageScripts/start.js
@@ -1,1 +1,19 @@
-console.log("Start script running");
+
+//Check for checkbox every 1 second
+var interval = setInterval(function(){
+	if($(".popupContent").length > 0){
+		//It popped up, so we close it :-D
+		var content = $(".popupContent > div > div > .modal-body > p").text();
+		//We dont want to close the wrong popup.
+		if(content == "Du har ikke rettigheder til at udføre denne service" || content == "Access denied for to this service"){
+			debugLog("Found error popup: " + content);
+			var button = $(".popupContent > div > div > div > .modal-footer > button");
+			//AAAAND click
+			button.click();
+			//Now that it's gone, we won't check anymore
+			//clearInterval(interval);
+		}
+
+	}
+
+}, 1000);
